198245b028e74c85f28d798d54e2f85bd1da4d2b
IsaacMagno <isaacmagno98@gmail.com>
2022-01-05 18:45:52 -0300

    backend-developing
    Mudanças a serem submetidas:
    new file:   back-end/src/controllers/costumerController.js
    new file:   back-end/src/controllers/sellerController.js
    modified:   back-end/src/database/config/config.js
    modified:   back-end/src/database/models/sale.js
    modified:   back-end/src/routes/routes.js
    new file:   back-end/src/services/costumerService.js
    new file:   back-end/src/services/sellerService.js

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -8,7 +8,12 @@ const jwtValidation = require('../middlewares/jwtValidation');
 const { login } = require('../controllers/loginController');
 const { register } = require('../controllers/registerController');
 const { ordersList } = require('../controllers/customerOrdersController');
+const sellerController = require('../controllers/sellerController');
+const customerController = require('../controllers/costumerController');
 
+
+router.get('/seller/orders', sellerController.getAll);
+router.post('/customer/orders', customerController.createOrder);
 router.post('/login', login);
 router.post('/register', register);
 router.post('/customer/orders', ordersList);
