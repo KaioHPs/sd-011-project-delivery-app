3542a8c28e4e4f651e66c8779212dde263778107
IsaacMagno <isaacmagno98@gmail.com>
2022-01-11 16:22:25 -0300

    eslint correctly

--- back-end/src/services/costumerService.js
+++ back-end/src/services/costumerService.js
@@ -2,19 +2,19 @@ const { sale } = require('../database/models/index');
 
 const createOrder = async (order) => {
   const { 
-    user_id, seller_id, 
-    total_price, delivery_address, 
-    delivery_number, sale_date,
+    user_id: userId, seller_id: sellerId, 
+    total_price: totalPrice, delivery_address: deliveryAddress, 
+    delivery_number: deliveryNumber, sale_date: saleDate,
   } = order;
   
   const orderCreated = sale
     .create({
-      userId: user_id,
-      sellerId: seller_id,
-      totalPrice: total_price,
-      deliveryAddress: delivery_address,
-      deliveryNumber: delivery_number,
-      saleDate: sale_date,
+      userId,
+      sellerId,
+      totalPrice,
+      deliveryAddress,
+      deliveryNumber,
+      saleDate,
       status: 'pendente',
     });
 
