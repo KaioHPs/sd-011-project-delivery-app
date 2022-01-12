1d853e4626113b04b828f33e7f9fb3b06b3bc959
IsaacMagno <isaacmagno98@gmail.com>
2022-01-12 16:45:07 -0300

    config fix

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV || "test";
 
 const suffix = {
   prod: "",
@@ -14,7 +14,7 @@ const options = {
   host: process.env.MYSQL_HOST || 'mysql',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.dev}`,
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
