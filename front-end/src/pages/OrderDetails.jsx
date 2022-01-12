faa10f98cd74ea88d0a816683b5b53fbdc444f7c
IsaacMagno <isaacmagno98@gmail.com>
2022-01-12 16:02:28 -0300

    seller completed

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -13,6 +13,10 @@ import {
 const OrderDetails = () => {
   const [order, setOrder] = useState();
   const { id } = useParams();
+  const entregue = 'ENTREGUE';
+  const preparando = 'PREPARANDO';
+  const transito = 'EM TRANSITO';
+  const pendente = 'PENDENTE';
 
   useEffect(() => {
     const getOrder = async () => {
@@ -23,6 +27,33 @@ const OrderDetails = () => {
 
     getOrder();
   }, [id]);
+
+  const updateStatus = async (newStatus) => {
+    const statusUpdated = await axios.put(`http://localhost:3001/seller/orders/${id}`, { status: newStatus })
+      .then((o) => o.data);
+
+    console.log(statusUpdated);
+
+    window.location.reload();
+  };
+
+  const preparingButton = () => {
+    const actualStatus = order.status;
+    if (actualStatus === preparando) return true;
+    if (actualStatus === entregue) return true;
+    if (actualStatus === transito) return true;
+
+    return false;
+  };
+
+  const dispatchButton = () => {
+    const actualStatus = order.status;
+    if (actualStatus === pendente) return true;
+    if (actualStatus === transito) return true;
+    if (actualStatus === entregue) return true;
+
+    return false;
+  };
 
   if (order) {
     const date = new Date(order.saleDate).toLocaleDateString();
@@ -40,12 +71,17 @@ const OrderDetails = () => {
           <button
             data-testid={ `${preparingCheck}${id}` }
             type="button"
+            onClick={ () => { updateStatus(preparando); } }
+            disabled={ preparingButton() }
           >
             PREPARAR PEDIDO
           </button>
           <button
             data-testid={ `${dispatchCheck}${id}` }
             type="button"
+            onClick={ () => { updateStatus(transito); } }
+            disabled={ dispatchButton() }
+
           >
             SAIU PARA ENTREGA
           </button>
